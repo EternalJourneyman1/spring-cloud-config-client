d29d00a5528a4e47f94d3e14498602f2afad90a5
Demetrious Robinson <demetriousr@demetriousr-a02.vmware.com>
2022-09-27 20:32:28 -0700

    Example Spring Cloud Config Client

---
+++ src/main/kotlin/dev/robinsond/config/client/ClientApplication.kt
@@ -0,0 +1,34 @@
+package dev.robinsond.config.client
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class ClientApplication {
+
+	@Value("\${user.phone}")
+	private val phone: String? = null
+
+	@Value("\${user.role}")
+	var role: String? = "not config server"
+
+
+	@GetMapping(
+		value = ["/profile/{firstName}"],
+		produces = [MediaType.TEXT_PLAIN_VALUE]
+	)
+	fun getUserRole(@PathVariable firstName: String = "John Doe"): String {
+		return "Welcome $firstName You are currently assigned as a $role, so we will give you a $phone"
+	}
+}
+
+fun main(args: Array<String>) {
+	runApplication<ClientApplication>(*args)
+}
